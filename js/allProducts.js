5c066be3e60a612da78adb9948c8ce221e07b77e
Liza-Sim <elizabet96@ukr.net>
2023-01-18 16:04:42 +0200

    edited the block with all products

--- js/allProducts.js
+++ js/allProducts.js
@@ -5,14 +5,19 @@ function showAllProduct() {
     document.getElementById('allProductPage').classList.remove('hidden');
     const allProducts = document.getElementById('allProducts');
     let index = 0;
+    let availability = 'in stock'
+    // тут должна быть функция которая смотрит наличие и соотв. и меняет цвет текста
     for (let i = 0; i < data.length; i++) {
         for (let j = 0; j < data[i].products.length; j++){
-            index++
-            createElement('div', { id: index , className: data[i].name, }, null, null, allProducts);
+            createElement('div', { id: index , className:data[i].name, }, null, null, allProducts);
             let elem = document.getElementById(index);
+            elem.classList.add('allProducts-card')
             createElement('span', { className: 'allProductName'}, null, data[i].products[j].name, elem);
+            createElement('img', { src: `images/${data[i].name}/${data[i].products[j].img}`, className: 'allProductImg' }, null, null, elem);
             createElement('span', { className: 'allProductPrice' }, null, `${data[i].products[j].price} $`, elem);
-            createElement('img', { src: `images/${data[i].name}/${data[i].products[j].img}` }, null, null, elem);
+            createElement('span', { className: 'allProductAvailability' }, null, availability, elem);
+            createElement('button', { type: 'button', className: 'allProduct_btn'  }, null, 'explore' , elem);
+            index++
         }
     }
 }
