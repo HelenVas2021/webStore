27e3cf8db7510b7381037974daab0579f1cb2481
Tetiana <105989526+TetianaTse@users.noreply.github.com>
2023-01-24 23:35:45 +0200

    added sort categories

--- js/category_menu.js
+++ js/category_menu.js
@@ -16,34 +16,81 @@ function showCategories() {
 showCategories();
 // взаимодеймтвие с категориями
 function showProducts(event) {
+    for (let i = 0; i < pageArr.length; i++) {
+        document.getElementById(pageArr[i]).classList.add('hidden');
+    }
     categoryBlock.classList.remove('hidden');
-    mainPage.classList.add('hidden');
-    allProducts.classList.add('hidden');
-    about.classList.add('hidden');
-    contacts.classList.add('hidden');
 
     const categoryIndex = event.target.getAttribute('data-category');
     const products = arrCategories[categoryIndex].products;
     const containerProduct = document.querySelector('.categories_card');
     containerProduct.innerHTML = '';
     
-    let availability = 'on sale';
-    
+    let availability;
     for(let i = 0; i < products.length; i++) {
+        if (products[i].availability === 0) {
+            availability = 'is expected';
+        } else {
+            availability = 'on sale';
+        }
         const elem = createElement(
             'div', 
-            {'data-product': i, 'className': arrCategories[categoryIndex].name + ' allProducts-card _mod_size', 'data-category': categoryIndex}, 
+            {'id': i, 'className': arrCategories[categoryIndex].name + ' allProducts-card _mod_size', 'data-category': categoryIndex, 'data-price': products[i].price}, 
             {click: showDetails}, 
             null,
             containerProduct);
             createElement('span', { className: 'allProductName'}, null, products[i].name, elem);
             createElement('img', { src: `${products[i].main_images}`, className: 'allProductImg' }, null, null, elem);
             createElement('span', { className: 'allProductPrice' }, null, `${products[i].price} UAN`, elem);
-            createElement('span', { className: 'allProductAvailability' }, null, availability, elem);
+            createElement('span', { id: `availability${i}`, className: 'allProductAvailability' }, null, availability, elem);
             createElement('button', { type: 'button', className: 'allProduct_btn'  }, null, 'explore' , elem);
+            let card = document.getElementById(i);
+            if (availability === 'is expected') {  
+                let elem = document.getElementById(`availability${i}`);
+                elem.classList.add('filter-notAviability');
+                card.classList.add('allProducts-card__notActive');
+            }
       }
   }
   function showDetails() {
 
   }
+
+// сортировка
+document.getElementById('ascending').addEventListener('click', () => {
+    sortAscending ('data-price');
+    document.getElementById('ascending').classList.add('activ_btn_sort');
+    document.getElementById('descending').classList.remove('activ_btn_sort');
+});
+document.getElementById('descending').addEventListener('click', () => {
+    sortDescending('data-price');
+    document.getElementById('descending').classList.add('activ_btn_sort');
+    document.getElementById('ascending').classList.remove('activ_btn_sort');
+});
+function sortAscending(sortType) {
+    let parent = document.querySelector('.categories_card');
+    for(let i = 0; i < parent.children.length; i++) {
+        for(let j = i; j < parent.children.length; j++) {
+            if (+parent.children[i].getAttribute(sortType) > +parent.children[j].getAttribute(sortType)) {
+                replaceNode = parent.replaceChild(parent.children[j], parent.children[i]);
+                insertAfter(replaceNode, parent.children[i]);
+            }
+        }
+    }
+}
+function sortDescending(sortType) {
+    let parent = document.querySelector('.categories_card');
+    for(let i = 0; i < parent.children.length; i++) {
+        for(let j = i; j < parent.children.length; j++) {
+            if (+parent.children[i].getAttribute(sortType) < +parent.children[j].getAttribute(sortType)) {
+                replaceNode = parent.replaceChild(parent.children[j], parent.children[i]);
+                insertAfter(replaceNode, parent.children[i]);
+            }
+        }
+    }
+}
+function insertAfter(elem, refElem) {
+    return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
+}
+
 
--- js/main_app.js
+++ js/main_app.js
@@ -1,4 +1,5 @@
 // для скрытия и показа блоков
+let pageArr = ['mainPage', 'category', 'allProductPage', 'about', 'contacts'];
 const mainPage = document.querySelector('#mainPage');
 const categoryBlock = document.querySelector('#category');
 const allProducts = document.getElementById('allProductPage');
@@ -6,16 +7,14 @@ const about = document.querySelector('#about');
 const contacts = document.querySelector('#contacts');
 
 document.querySelector('#aboutBtn').addEventListener('click', ()=> {
+    for (let i = 0; i < pageArr.length; i++) {
+        document.getElementById(pageArr[i]).classList.add('hidden');
+    }
     about.classList.remove('hidden');
-    mainPage.classList.add('hidden');
-    categoryBlock.classList.add('hidden');
-    allProducts.classList.add('hidden');
-    contacts.classList.add('hidden');
 });
 document.querySelector('#contactsBtn').addEventListener('click', ()=> {
+    for (let i = 0; i < pageArr.length; i++) {
+        document.getElementById(pageArr[i]).classList.add('hidden');
+    }
     contacts.classList.remove('hidden');
-    mainPage.classList.add('hidden');
-    categoryBlock.classList.add('hidden');
-    allProducts.classList.add('hidden');
-    about.classList.add('hidden');
 });
