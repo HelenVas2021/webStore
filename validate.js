4999363ccf2a903685e0fee77804730725b78939
Helen D <johndoe@example.com>
2023-02-04 16:55:55 +0200

    change

---
+++ validate.js
@@ -0,0 +1,46 @@
+
+const validate = {
+	formName: /[A-Za-z]{1,15}/,
+	email: /^([A-Za-z0-9_-]+\.)*[A-Za-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/,
+	phoneNumber: /^[0-9]{10}$/,
+	country: /[A-Za-z]{1,15}/,
+	city: /[A-Za-z]{1,15}/,
+	street: /[A-Za-z]{1,15}/,
+	house: /[0-9]{1,3}/,
+	apartment: /[0-9]{1,3}/,
+}
+
+function checkValidate(userInfo) {
+	let isError = false;
+	for (let key in validate) {
+		if (validate[key].exec(userInfo[key])) {
+			continue;
+		} else {
+			let elem = document.getElementById(key);
+			const error = document.createElement('span');
+			error.textContent = 'Change your info!';
+			elem.after(error);
+			error.setAttribute('class','text-field__message');
+			isError = true;
+
+		}
+	}
+	return isError;
+}
+
+function defaultValue() {
+	let items = document.querySelectorAll('span.text-field__message');
+	items.forEach(elem => elem.remove());
+	
+}
+
+
+
+
+// for (let key in userInfo) {
+// 	let item = document.getElementById(key);
+// 	console.log(item);
+// 	if (item.contains(span)) {
+// 		item.className.remove('span');
+// 	}
+// }
